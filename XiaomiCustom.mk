bf2281aac4ceeb4cf3503a70c0614e287afa15be
Arian <39061882+LMMFHHO@users.noreply.github.com>
2018-08-28 13:05:21 +0200

    XiaomiCustom: add DigitalWellbeing by Google to the settings.
    Base for this work:
    XDA (https://www.xda-developers.com/get-google-pixel-digital-wellbeing-essential-phone-android-pie/)
    APKMirror (https://www.apkmirror.com/apk/google-inc/digital-wellbeing/digital-wellbeing-0-2-207862754-release/digital-wellbeing-0-2-207862754-android-apk-download/download/)

--- XiaomiCustom.mk
+++ XiaomiCustom.mk
@@ -14,3 +14,22 @@
 PRODUCT_PACKAGES := \
 	GoogleCamera \
 	XiaomiParts
+# DigitalWellbeing
+PRODUCT_COPY_FILES += \
+	$(DWB_PATH)/DigitalWellbeing.apk:system/priv-app/DigitalWellbeing/DigitalWellbeing.apk
+	$(DWB_PATH)/lib/arm/libfyusenet.so:system/priv-app/DigitalWellbeing/lib/arm/libfyusenet.so
+	$(DWB_PATH)/lib/arm/libHW_Pano_f.so:system/priv-app/DigitalWellbeing/lib/arm/libHW_Pano_f.so
+	$(DWB_PATH)/lib/arm/libjni_mrc_cg_filters_camera2.so:system/priv-app/DigitalWellbeing/lib/arm/libjni_mrc_cg_filters_camera2.so
+	$(DWB_PATH)/lib/arm/libIvw35.so:system/priv-app/DigitalWellbeing/lib/arm/libIvw35.so
+	$(DWB_PATH)/lib/arm/libmorpho_memory_allocator.so:system/priv-app/DigitalWellbeing/lib/arm/libmorpho_memory_allocator.so
+	$(DWB_PATH)/lib/arm/libJniVoiceCapture.so:system/priv-app/DigitalWellbeing/lib/arm/libJniVoiceCapture.so
+	$(DWB_PATH)/lib/arm/libmorpho_panorama_gp.so:system/priv-app/DigitalWellbeing/lib/arm/libmorpho_panorama_gp.so
+	$(DWB_PATH)/lib/arm/libTargetTracking.so:system/priv-app/DigitalWellbeing/lib/arm/libTargetTracking.so
+	$(DWB_PATH)/lib/arm/libscanner.so:system/priv-app/DigitalWellbeing/lib/arm/libscanner.so
+	$(DWB_PATH)/lib/arm/libtrack.so:system/priv-app/DigitalWellbeing/lib/arm/libtrack.so
+	$(DWB_PATH)/lib/arm/libjni_front_panorama_camera2.so:system/priv-app/DigitalWellbeing/lib/arm/libjni_front_panorama_camera2.so
+	$(LOCAL_PATH)/DigitalWellbeing/system/etc/sysconfig/wellbeing-enabler.xml:system/etc/sysconfig/wellbeing-enabler.xml
+
+# DigitalWellbeing permissions
+PRODUCT_COPY_FILES += \
+	$(VENDOR_PATH)/DigitalWellbeingApp/com.google.android.apps.wellbeing.permissions.xml:system/etc/permissions/com.google.android.apps.wellbeing.permissions.xml
