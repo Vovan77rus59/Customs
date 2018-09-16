3be48fe5f1597cc1db71010d5255a98fe66f13a9
arian <arian.kulmer@web.de>
2018-09-16 19:41:24 +0200

    Specials: add Google Pixel 2`s bootanimation and fix GCam

--- XiaomiCustom.mk
+++ XiaomiCustom.mk
@@ -11,3 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# include apps
+PRODUCT_PACKAGES += \
+GoogleCamera
+
+# Pixel Bootanimation
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/PixelBootanimation/system/media/bootanimation.zip:system/media/bootanimation.zip
